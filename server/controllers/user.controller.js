443fa6f3e7e344007605e7e641bb4a9b8a8683fe
krAryn <aj619119orp@gmail.com>
2025-05-06 14:01:35 +0530

    Add login controller

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -9,30 +9,64 @@ export const registerUser = async (req, res) => {
 
         if (!name || !email || !password) {
             console.log(name, " ", email, " ", password, " ")
-            return res.json({ success: false, message: "Missing details!" })
+            return await res.json({ success: false, message: "Missing details!" })
         }
 
         const existingUser = await User.findOne({ email })
 
         if (existingUser) {
-            return res.json({ success: false, message: "User Already Exists!" })
+            return await res.json({ success: false, message: "User Already Exists!" })
         }
 
         const hashedPassword = await bcrypt.hash(password, 10)
 
         const user = await User.create({ name, password: hashedPassword, email })
-        const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: "7d" })
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: "7d" })
 
         res.cookie("token", token, { 
             httpOnly: true,              // Prevents browser js in clinet to access or modify it
             maxAge: 7 * 24 * 60 * 60 * 1000,
         })
 
-        res.json({success: true, message: "User Registration Complete", user: {name, email, password}})
+        return await res.json({success: true, message: "User Registration Complete", user: {name, email, password}})
 
+    } catch (error) {
+        console.log(error.message)
+        return awaitres.json({success: false, message: error.message})
+    }
+}
+
+// Path: /api/user/login
+export const loginUser = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        if (!email || !password) {
+            return await res.json({success: false, message: "Email address and password are required!"})
+        }
+
+        const existingUser = await User.findOne({email})
+
+        if (!existingUser) {
+            return await res.json({success: false, message: "User not found!"})
+        }
+
+        const passwordMatched = await bcrypt.compare(password, existingUser.password)
+
+        if (!passwordMatched) {
+            return await res.json({success: false, message: "Invalid Credentials!"})
+        }
+
+        const token = jwt.sign({userId: existingUser._id}, process.env.JWT_SECRET, {expiresIn: "7d"});
+
+        res.cookie("token", token, {
+            httpOnly: true,
+            maxAge: 7 * 24 * 60 * 60 * 1000
+        })
+
+        return await res.json({success: true, message: "User is Valid "})
     } catch (error) {
         console.log(error.message)
         res.json({success: false, message: error.message})
     }
 }
-
