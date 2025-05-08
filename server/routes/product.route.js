1301302570d4ea9ed19d4758bdee5c184ba23c12
krAryn <aj619119orp@gmail.com>
2025-05-08 12:41:56 +0530

    Add product routesr

--- server/routes/product.route.js
+++ server/routes/product.route.js
@@ -1,9 +1,13 @@
 import express from "express"
-import { addProduct } from "../controllers/products.controller.js"
+import { addProduct, changeStock, productById, productList } from "../controllers/products.controller.js"
 import upload from "../config/multer.config.js"
+import authSeller from "../middlewares/authSeller.js"
 
 const productRoute = express.Router()
 
-productRoute.post("/add", upload.array("images", 2), addProduct)
+productRoute.post("/add", authSeller,upload.array("images", 2), addProduct)
+productRoute.get("/list", productList)
+productRoute.get("/id", productById)
+productRoute.get("/stock", changeStock)
 
 export default productRoute
