4c92adc4c678207d35ae74666b4b0e36329c0b63
krAryn <aj619119orp@gmail.com>
2025-05-08 13:35:45 +0530

    Minor Updates

---
+++ server/middlewares/user.auth.js
@@ -0,0 +1,29 @@
+// Middleware for protected paths
+import jwt from "jsonwebtoken"
+
+const authUser = async (req, res, next) => {
+
+    try {
+        const {token} = req.cookies
+        console.log("Token: ", token)
+    
+        if (!token) {
+            return res.json({success: false, message: "Token not defined!"})
+        }
+    
+        const user = jwt.verify(token, process.env.JWT_SECRET)
+    
+        if (!user) {
+            return res.json({success: false, message: "Token verification failed!"})
+        }
+    
+        req.body.userId = user.userId
+        next()
+    } catch (error) {
+        console.log(error.message)
+        return await res.json({success: false, message: error.message})
+    }
+
+}
+
+export default authUser
